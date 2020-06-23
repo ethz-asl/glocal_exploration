8e1628307c018ae0f2ca38d9b3170a782b3c242e
Schmluk <schmluk@student.ethz.ch>
2020-06-23 17:43:28 +0200

    adapt and rename map interface

--- glocal_exploration/include/glocal_exploration/mapping/map_base.h
+++ glocal_exploration/include/glocal_exploration/mapping/map_base.h
@@ -1,20 +1,27 @@
 #ifndef GLOCAL_EXPLORATION_MAPPING_MAP_INTERFACE_H_
 #define GLOCAL_EXPLORATION_MAPPING_MAP_INTERFACE_H_
 
+#include <memory>
+
 #include "glocal_exploration/common.h"
+#include "glocal_exploration/planning/state_machine.h"
 
 namespace glocal_exploration {
 /**
  * Defines the interface of a map module that is needed by the planner.
  */
-class MapInterface {
+class MapBase {
  public:
   // Defines a baseclass for map configurations
   struct Config {
     virtual ~Config() = default;
   };
-  MapInterface() = default;
-  virtual ~MapInterface() = default;
+
+  enum VoxelState { Unknown, Occupied, Free };
+
+  // Constructors
+  explicit MapBase(std::shared_ptr<StateMachine> state_machine) : state_machine_(std::move(state_machine)) {}
+  virtual ~MapBase() = default;
 
   /* Setup */
   // Can pass derived configs here by base pointer to setup the map.
@@ -24,10 +31,14 @@ class MapInterface {
   virtual double getVoxelSize() = 0;
 
   /* Local planner */
-  virtual bool isTraversableInActiveSubmap(const Eigen::Vector3d &position, const Eigen::Quaterniond &orientation) = 0;
+  virtual bool isTraversableInActiveSubmap(const Eigen::Vector3d &position,
+                                           const Eigen::Quaterniond &orientation = Eigen::Quaterniond::Identity()) = 0;
   // sets *center to the center of the voxel that contains point. Voxels are referred in the planner by their centers.
   virtual bool getVoxelCenterInLocalArea(Eigen::Vector3d *center, const Eigen::Vector3d &point) = 0;
-  virtual bool isObservedInLocalArea(const Eigen::Vector3d &point) = 0;
+  virtual VoxelState getVoxelStateInLocalArea(const Eigen::Vector3d &point) = 0;
+
+ protected:
+  const std::shared_ptr<StateMachine> state_machine_;
 };
 
 } // namespace glocal_exploration
