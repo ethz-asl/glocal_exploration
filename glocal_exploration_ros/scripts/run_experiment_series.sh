6b3217621a28051937786adc14b8f80ab9992e91
Schmluk <schmluk@student.ethz.ch>
2020-09-15 17:10:01 +0200

    more manageable series

--- glocal_exploration_ros/scripts/run_experiment_series.sh
+++ glocal_exploration_ros/scripts/run_experiment_series.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # *** Args (need to be set) ***
-n_experiments=1
+n_experiments=2
 target_dir="/home/lukas/Documents/Glocal/Data/test2"		# Can reuse same dir to add experiments
 clear_voxblox_maps=true		# Irreversibly remove maps after evaluation to save disk space
 launch_file="run_maze"
+duration=30
+frequency=30
 
 # *** Run experiments ***
 echo "Starting experiment series '$launch_file' of ${n_experiments} runs at '${target_dir}'!"
@@ -18,7 +20,7 @@ fi
 for (( i=1; i<=n_experiments; i++ ))
 do  
   # run experiment
-  roslaunch glocal_exploration_ros $launch_file.launch data_path:=$target_dir record_data:=true
+  roslaunch glocal_exploration_ros $launch_file.launch data_path:=$target_dir record_data:=true time_limit:=$duration data_interval:=$frequency
   # evaluate
   roslaunch glocal_exploration_ros evaluate_experiment.launch target_directory:=$target_dir method:=recent series:=false clear_voxblox_maps:=$clear_voxblox_maps evaluate:=true
 done
