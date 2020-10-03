256576d60d17ad77bf2f3ad6be58f109b7e6db18
Victor Reijgwart <victor_reijgwart@hotmail.com>
2020-10-04 00:23:43 +0200

    New batch of experiments

--- glocal_exploration_ros/scripts/run_experiment_series.sh
+++ glocal_exploration_ros/scripts/run_experiment_series.sh
@@ -40,36 +40,36 @@ duration=15      #min
 target_dir="/home/victor/data/glocal/automated_tests/glocal/"
 launch_file="run_maze"  # run_maze, active_3d_run_maze
 
-drift="maze/drift_4"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_4"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_3"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_3"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_2"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_2"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_1"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_1"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_none"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_0"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
 # Active 3D
 target_dir="/home/victor/data/glocal/automated_tests/active_3d/"
 launch_file="active_3d_run_maze"  # run_maze, active_3d_run_maze
 
-drift="maze/drift_4"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_4"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_3"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_3"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_2"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_2"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_1"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_1"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
 
-drift="maze/drift_none"  # drift_none, drift_1, drift_2, drift_3, drift_4
+drift="maze/drift_0"  # drift_0, drift_1, drift_2, drift_3, drift_4
 run_experiments
